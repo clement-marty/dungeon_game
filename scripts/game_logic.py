a687889fab1fab0351c8db283ba91c684a9b998b
Clément Marty <clement_marty@outlook.fr>
2024-10-22 09:16:23 +0200

    Made Player class child of an Entity class

--- scripts/game_logic.py
+++ scripts/game_logic.py
@@ -22,14 +22,13 @@ class GameLogic:
         cls.OBSTACLES_VMATRIX = obstacles_vmatrix
 
 
-    class Player:
-            
-        def __init__(self, max_health: int = 100, max_energy: int = 100) -> None:
+
+    class Entity:
+
+        def __init__(self, max_health: int) -> None:
             self.position: list[int, int]
             self.max_health = max_health
-            self.max_energy = max_energy
-            self._health = max_health
-            self._energy = max_energy
+            self.health = max_health
 
         def _move(self, direction: tuple[int, int]) -> None:
             new_position = (self.position[0] + direction[0], self.position[1] + direction[1])
@@ -37,17 +36,25 @@ class GameLogic:
                 (GameLogic.OBSTACLES_VMATRIX[*new_position][0] is None or GameLogic.DUNGEON_GRID[*new_position] != 1):
                 self.position = new_position
 
+        @property
+        def health(self) -> int:
+            return self._health
+        def reduce_health(self, amount: int) -> None: self._health = max(0, self._health - amount)
+
+
+    class Player(Entity):
+            
+        def __init__(self, max_health: int = 100, max_energy: int = 100) -> None:
+            super().__init__(max_health)
+            self.max_energy = max_energy
+            self._energy = max_energy
+
         def move_up(self) -> None: self._move((0, -1))
         def move_down(self) -> None: self._move((0, 1))
         def move_left(self) -> None: self._move((-1, 0))
         def move_right(self) -> None: self._move((1, 0))
 
         @property
-        def health(self) -> int:
-            return self._health
-        @property
         def energy(self) -> int:
             return self._energy
-
-        def reduce_health(self, amount: int) -> None: self._health = max(0, self._health - amount)
         def reduce_energy(self, amount: int) -> None: self._energy = max(0, self._energy - amount)
